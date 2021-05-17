c392c9c341ef03f93a7165a47fbb0c206a8bd63d
hung <bacthaychemgio11@gmail.com>
2021-05-17 11:26:15 +0700

    test qhe

---
+++ 1-source/app/Http/Controllers/Categories.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Categories extends Controller
+{
+    // public  function getCategory(){
+    //     return $this->morphTo();
+    // }
+}
